cc2c4933421f510dadde623e15df28d193954494
BTFitti <bruno.fitti734@live.com>
2025-02-04 11:49:09 -0300

    teste

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,9 @@
+export default function Contato(){
+    return(
+        <div>
+            <h1>
+                N: 
+            </h1>
+        </div>
+    )
+}
---
+++ src/app/teste.tsx
@@ -0,0 +1,8 @@
+
+export default function Teste(){
+    return(
+        <div>
+            <h1>Nig</h1>
+        </div>
+    )
+}
